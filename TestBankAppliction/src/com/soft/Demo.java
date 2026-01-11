983ea904723bbd3c93e810954546c4d6d11609fa
adarsh7888 <adarshi7888@gmail.com>
2026-01-11 10:07:56 +0530

    new canages in demo

--- TestBankAppliction/src/com/soft/Demo.java
+++ TestBankAppliction/src/com/soft/Demo.java
@@ -2,9 +2,9 @@ package com.soft;
 
 public class Demo {
 public static void main(String[] args) {
-	System.out.println("Welcome to java program");{
-		
-	}
+	System.out.println("Welcome to java program");
+	System.out.println("Welcome to  program");
+	
 }
 
 }
