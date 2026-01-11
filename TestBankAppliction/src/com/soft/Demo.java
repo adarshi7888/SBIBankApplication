ab00d67fa5f945def74243138adc904cdeb4368e
adarshi7888 <adarshi7888@gmail.com>
2026-01-11 10:10:13 +0530

    Update Demo.java

--- TestBankAppliction/src/com/soft/Demo.java
+++ TestBankAppliction/src/com/soft/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 public static void main(String[] args) {
 	System.out.println("Welcome to java program");
 	System.out.println("Welcome to  program");
+	System.out.println("Welcome to  irise software");
 	
 }
 
